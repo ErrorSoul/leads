ec53943b48348219667dd9fece37d52eb8acf2c5
Abu Sultanov <neurofag@gmail.com>
2014-08-22 03:00:54 +0400

    with after_save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@
 class UsersController < ApplicationController
   respond_to :json
   #after_action :message, only: [:create]
+  after_action :data_send, only: [:create]
+
 
   #
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.delay.mess
+      #@user.delay.mess
       render json: { message: ["registration complete"]}
     else
       render json: {message: @user.errors.full_messages}
@@ -18,6 +20,10 @@ class UsersController < ApplicationController
  
    
     private
+
+    def data_send
+      @user.mess
+    end
     def user_params
       params.require(:user).permit(:first_name, :last_name, :birthdate, :email, :country, :password)
     end
